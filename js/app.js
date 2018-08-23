ec9ea2ff2115c4b9c26a5e12e205674ed7dee732
ali frd <alifrd49@gmail.com>
2018-08-23 15:47:46 +0430

    added app js with event listner and btn disable

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//Variable 
+const sndbtn = document.getElementById('sendBtn');
+const 
+
+//Event Listner
+eventListners();
+
+function eventListners(){
+    document.addEventListener('DOMContentLoaded', appinit)
+}
+
+
+//Function
+
+function appinit(){
+    sndbtn.disabled = true ;
+}
